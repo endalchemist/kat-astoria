854cf9cce85f5b6094122df068973621166bd072
endalchemist <cherwj71@uwosh.edu>
2017-12-28 02:46:40 -0600

    Add files via upload

---
+++ TestGUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGUI : MonoBehaviour {
+
+	private BaseToonClass class1 = new BaseMainArcher();
+	private BaseToonClass class2 = new BaseMainDivineMage();
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI (){
+		GUILayout.Label();
+
+	}
+}
