18ff21cc1690e0fdeb33b73d7e02db63263e3376
sigmatheta <34907294+sigmatheta@users.noreply.github.com>
2017-12-31 01:21:43 -0600

    added key presses..
    need to add more

--- scripts/Toon Movement/ToonMovement.cs
+++ scripts/Toon Movement/ToonMovement.cs
@@ -21,6 +21,42 @@ public class ToonMovement :  MonoBehaviour {
         if(Input.GetKey("a")){
             Debug.Log("Pressing A key");
         }
+		if(Input.GetKeyDown("1")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("2")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("3")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("4")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("5")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("6")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("7")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("8")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("9")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("0")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("-")){
+            Debug.Log("Pressing A key");
+        }
+		if(Input.GetKeyDown("=")){
+            Debug.Log("Pressing A key");
+        }
 
     }
 
