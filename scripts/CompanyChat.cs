2f03ffadd31c326483f1f32029910f8b422304b9
sigmatheta <34907294+sigmatheta@users.noreply.github.com>
2018-01-03 22:05:27 -0600

    chat

--- scripts/CompanyChat.cs
+++ scripts/CompanyChat.cs
@@ -10,21 +10,13 @@
 *get example humanoid object frm lane. camera angle n collisions
 *get toon setup complete
 *goal end date for setup jan 11
-*bring on second animator
-*determine exact stakes in game company/revenue from projects
+*
+*
 **SEE BASE PLAYER GUI LINE 76/77 needs to be renamed to whatever our "tset" file is from vids
 */
 
-//oh shit so when im connected to you it will only save changes on your computer
-//so if i download the github app can we have it change the files directly to the master regardless of who opens said files?
-// working on episode 13 rn
-//yeah let me work on the github so that we can just upload all the files like you said instead of just the zip
-//and yeah you can set up a folder on your pc so that it syncs with the github repo
-//i think you just have to click a button on the app to do a commit whenever you've completed a file or whatever
-// downloading desktop app rn
-// ill have to save n move the files hold on
-//alright sick, go ahead and put those files you worked on in that cloned folder and sync, ill put a screenshot in twitchchat
-//hello
-// ye experimenting with the back n forth?
-//yup exactly
-// seems to  work this way
+//accces lanes maps and models via google drive?
+// assign models and icon images to all weapons
+// monsters
+// quests
+// story
